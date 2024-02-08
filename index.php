81c764fc9f716e449ce92c26bf3fd69713fae25a
gabrielebottari <gabriele.bottari1@gmail.com>
2024-02-08 13:34:50 +0100

    ADD base structure

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Template PHP</title>
+        <title>PHP To Do List JSON</title>
     </head>
     <body>
 
